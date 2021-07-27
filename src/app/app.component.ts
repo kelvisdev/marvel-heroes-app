afa5a3f4479f6a1b0f18c96b22486b7fde07bf71
Kelvis Borges <kelvisbcc@gmail.com>
2021-07-26 20:01:30 -0400

    add base style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<div class="container"><router-outlet></router-outlet></div>',
 })
-export class AppComponent {
-  title = 'marvel-heroes-app';
-}
+export class AppComponent { }
